8008dbed4d6290a2a56691959115be6767975101
汪洋 <igiuify@gmail.com>
2020-09-07 22:58:56 +0800

    Initial Commit

---
+++ CTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CTest
+//
+//  Created by wangyang on 2020/9/7.
+//  Copyright © 2020 com.wy.personal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
